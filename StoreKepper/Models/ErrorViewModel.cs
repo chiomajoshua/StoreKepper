42b65b253e210724b00c813cb07ad70ba40d453d
Chioma Joshua <chiomajoshua@gmail.com>
2020-05-22 00:52:41 +0100

    Add project files.

---
+++ StoreKepper/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StoreKepper.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
